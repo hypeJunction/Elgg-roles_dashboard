e759a9b8d608ac7c347953d230c9cc92103fe906
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-05-09 10:48:09 +0200

    chore(languages): automatically add dashboard tab name translations

--- languages/en.php
+++ languages/en.php
@@ -5,7 +5,7 @@ $en = [];
 $roles = roles_get_all_selectable_roles();
 if (!empty($roles)) {
 	foreach ($roles as $role) {
-		$en["role::{$role->name}"] = $role->getDisplayName();
+		$en["dashboard::{$role->name}"] = $role->getDisplayName();
 	}
 }
 
